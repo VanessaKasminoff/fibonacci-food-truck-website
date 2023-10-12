28537f11c4890591bef91833c7afc859bb454fa8
Vanessa Kasminoff <sunnset07@gmail.com>
2023-10-12 09:52:58 -0700

    copied the code over from CodePen into the files.

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+$('.img-logo').fadeIn(3000);
+$('.menu-title').delay(2000).fadeIn(3000);
+$('.math-favs').delay(4000).fadeIn(3000);
+$('#location-nav').delay(6000).fadeIn(3000);
+$('#hours-nav').delay(6000).fadeIn(3000);
